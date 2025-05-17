000d49645e87b7a50fe2ac54bf34e34912ac330d
itali1 <143143318+itali1@users.noreply.github.com>
2025-05-17 01:20:06 -0700

    Add files via upload

--- graph.h
+++ graph.h
@@ -1,12 +1,48 @@
 #ifndef GRAPH_H
 #define GRAPH_H
+#include "heap.h"
 
 class Graph {
 public:
-    Graph(int vertices);
-    ~Graph();
-    void addEdge(int u, int v, int weight);
-    void primMST();  // Must print MST edges and total weight
+    Graph(int vertices) {
+        numVertices = vertices;
+        //adjacency matrix of size numVertices*numVertices is created
+        adjMatrix = new int *[numVertices];
+        for (int i = 0; i < numVertices; i++) {
+            adjMatrix[i] = new int[numVertices];
+            for (int j = 0; j < numVertices; j++) {
+                if(i == j) {
+                    adjMatrix[i][j] = 0;
+                }
+                else {
+                    adjMatrix[i][j] = INT_MAX;
+                }
+            }
+        }
+    }
+    ~Graph(){}
+    void addEdge(int u, int v, int weight) {
+        adjMatrix[u][v] = weight;
+        adjMatrix[v][u] = weight;
+    }
+    void primMST() {
+        // Must print MST edges and total weight
+        int min;
+        MinHeap a(numVertices); // initialize MinHeap of size numVertices
+        for(int i = 0; i < numVertices; i++) {
+            min = a.extractMin(); //returns the minKey at a certain index of KeyArray
+            for(int j = 0; j < numVertices; j++) {
+                if(adjMatrix[i][j] > 0) {
+                    a.decreaseKey(j, adjMatrix[i][j]);
+                }
+                if(adjMatrix[i][j] == min) {
+                    if(adjMatrix[i][j] != 0) {
+                        cout<<j<<" -- "<<i<<" ("<<min<<")"<<endl;
+                    }
+                }
+            }
+        }
+    }
 
 private:
     int** adjMatrix;
--- main.cpp
+++ main.cpp
@@ -2,16 +2,13 @@
 #include "graph.h"
 
 int main() {
-    Graph g(5);
-    g.addEdge(0, 1, 2);
-    g.addEdge(0, 3, 6);
-    g.addEdge(1, 2, 3);
-    g.addEdge(1, 3, 8);
-    g.addEdge(1, 4, 5);
-    g.addEdge(2, 4, 7);
-    g.addEdge(3, 4, 9);
-
-    g.primMST();
+    Graph g(4);              // Initialize graph with 4 vertices
+    g.addEdge(0, 1, 2);      // Add edge 0 - 1 with weight 2
+    g.addEdge(0, 2, 4);      // Add edge 0 - 2 with weight 4
+    g.addEdge(1, 2, 1);      // Add edge 1 - 2 with weight 1
+    g.addEdge(1, 3, 7);      // Add edge 1 - 3 with weight 7
+    g.addEdge(2, 3, 3);      // Add edge 2 - 3 with weight 3
+    g.primMST();             // Call Prim's algorithm
 
     return 0;
 }
